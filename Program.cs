84aa13a4faa986de95960fa57673a4032edfde6f
320 陳永修 <peterch@mirle.com.tw>
2019-07-08 21:01:24 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp_AOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var order = new Order();
+            order.Update("91", "Joey");
+            order.Delete("92");
+        }
+    }
+
+    internal class Order
+    {
+        public int Update(string id, string name)
+        {
+            Console.WriteLine($"Update order, id={id}, name={name}");
+            return 1;
+        }
+
+        public void Delete(string id)
+        {
+            Console.WriteLine($"Delete order, id={id}");
+        }
+    }
+}
