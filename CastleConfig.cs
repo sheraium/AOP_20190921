414ba015930c4e92bc4d7e9b207a63e148dba40d
320 陳永修 <peterch@mirle.com.tw>
2019-07-08 21:50:30 +0800

    只針對方法名字有 Update 的進行攔截

--- CastleConfig.cs
+++ CastleConfig.cs
@@ -1,4 +1,7 @@
-﻿using Castle.Core;
+﻿using System;
+using System.Linq;
+using Castle.Core;
+using Castle.DynamicProxy;
 using Castle.MicroKernel.Registration;
 using Castle.Windsor;
 
@@ -22,15 +25,36 @@ namespace ConsoleApp_AOP
                     .ImplementedBy<Order>().LifestyleTransient());
 
             // 額外註冊有加載攔截器的Order
+            //Container.Register(
+            //    Component.For<IOrder>()
+            //        .ImplementedBy<Order>().LifestyleTransient().Named("logOrder")
+            //        .Interceptors(InterceptorReference.ForType<LogInterceptor>()).Anywhere);
+
             Container.Register(
                 Component.For<IOrder>()
                     .ImplementedBy<Order>().LifestyleTransient().Named("logOrder")
-                    .Interceptors(InterceptorReference.ForType<LogInterceptor>()).Anywhere);
+                    .Interceptors(InterceptorReference.ForType<LogInterceptor>()).Anywhere
+                    .SelectInterceptorsWith(new OnlyUpdateMethodBeSelected()));
 
             Container.Register(
                 Component.For<ICustomer>()
                     .ImplementedBy<Customer>().LifestyleTransient()
                     .Interceptors(InterceptorReference.ForType<LogInterceptor>()).Anywhere);
+        }
+    }
+
+    internal class OnlyUpdateMethodBeSelected : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, System.Reflection.MethodInfo method, IInterceptor[] interceptors)
+        {
+            if (method.Name.Contains("Update"))
+            {
+                return interceptors;
+            }
+            else
+            {
+                return Enumerable.Empty<IInterceptor>().ToArray();
+            }
         }
     }
 }
