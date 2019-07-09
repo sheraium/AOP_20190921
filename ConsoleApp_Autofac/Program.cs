ec010eaea09cd903d2e7db687268b3930dfc92c3
320 陳永修 <peterch@mirle.com.tw>
2019-07-09 17:28:50 +0800

    新增 ConsoleApp_Autofac

---
+++ ConsoleApp_Autofac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp_Autofac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
