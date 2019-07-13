2b9cb7b90e5a19084ecb6aba92fb6a22f65bf1fc
320 陳永修 <peterch@mirle.com.tw>
2019-07-13 20:42:03 +0800

    Singleton

---
+++ ConsoleApp_Autofac_Singleton/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Autofac;
+
+namespace ConsoleApp_Autofac_Singleton
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<TheNewOne>().SingleInstance();
+            var container = builder.Build();
+            for (int i = 0; i < 3; i++)
+            {
+                Task.Run(() =>
+                {
+                    var theOne = container.Resolve<TheNewOne>();
+                    theOne.ShowUniqueKey();
+                });
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp_Autofac_Singleton/TheOne.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp_Autofac_Singleton
+{
+    public class TheOne
+    {
+        private Guid UniqueKey = Guid.NewGuid();
+        private static TheOne instance = null;
+        private static object syncRoot = new Object();
+
+        public static TheOne Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                            instance = new TheOne();
+                    }
+                }
+                return instance;
+
+            }
+        }
+
+        /// <summary>
+        /// 建構式
+        /// </summary>
+        private TheOne()
+        {
+            Thread.Sleep(2000);
+            Console.WriteLine("Constructor Executed");
+        }
+
+        public void ShowUniqueKey()
+        {
+            Console.WriteLine("Unique Key={0}", UniqueKey);
+        }
+    }
+}
