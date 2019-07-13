b5a4998ffe1a0012b776021b48f5e74484b41fc9
320 陳永修 <peterch@mirle.com.tw>
2019-07-13 21:37:51 +0800

    parameter

---
+++ ConsoleApp_Autofac_Parameter/MultiConstructor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp_Autofac_Parameter
+{
+    public class ArgW { }
+    public class ArgX { }
+    public class ArgY { }
+    public class ArgZ { }
+
+    public class MultiConstructor
+    {
+        public MultiConstructor(ArgW w)
+        {
+            Console.WriteLine("Constructor ArgW");
+        }
+        public MultiConstructor(int i, int j, ArgY y)
+        {
+            Console.WriteLine("Constructor int, int, ArgY");
+        }
+        public MultiConstructor(ArgY y, ArgZ z)
+        {
+            Console.WriteLine("Constructor ArgY, ArgZ");
+        }
+        public MultiConstructor(ArgX x)
+        {
+            Console.WriteLine("Constructor ArgX");
+        }
+        public MultiConstructor(ArgW w, ArgX x)
+        {
+            Console.WriteLine("Constructor ArgW, ArgX");
+        }
+        public MultiConstructor(ArgX x, ArgZ z)
+        {
+            Console.WriteLine("Constructor ArgX, ArgZ");
+        }
+    }
+}
---
+++ ConsoleApp_Autofac_Parameter/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Autofac;
+
+namespace ConsoleApp_Autofac_Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterType<MultiConstructor>();
+            builder.RegisterType<ArgW>();
+            IContainer container = builder.Build();
+            var obj1 = container.Resolve<MultiConstructor>(
+                new PositionalParameter(0, 1),
+                new PositionalParameter(1, 2),
+                new PositionalParameter(2, new ArgY())
+            );
+            var obj2 = container.Resolve<MultiConstructor>(
+                new TypedParameter(typeof(ArgY), new ArgY()),
+                new TypedParameter(typeof(ArgZ), new ArgZ())
+            );
+            var obj3 = container.Resolve<MultiConstructor>();
+            var obj4 = container.Resolve<MultiConstructor>(
+                new NamedParameter("x", new ArgX())
+            );
+            try
+            {
+                var obj5 = container.Resolve<MultiConstructor>(
+                    new NamedParameter("x", new ArgX()),
+                    new NamedParameter("z", new ArgX()));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
+            }
+            var obj6 = container.Resolve<MultiConstructor>(
+                new NamedParameter("i", 1),
+                new NamedParameter("y", new ArgX()));
+            Console.ReadLine();
+
+        }
+    }
+
+
+  
+}
