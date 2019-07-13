757f7fa7b823131113d33d318169fd375bff3842
320 陳永修 <peterch@mirle.com.tw>
2019-07-13 20:49:22 +0800

    lifetime scope container

---
+++ ConsoleApp_LifetimeScope/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Autofac;
+
+namespace ConsoleApp_LifetimeScope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ResourceMonster>();
+            var container = builder.Build();
+
+            var monster = container.Resolve<ResourceMonster>();
+            monster.Test();
+            Console.WriteLine("Before IContainer Dispose");
+            container.Dispose();
+            Console.WriteLine("After IContainer Dispose");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp_LifetimeScope/ResourceMonster.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp_LifetimeScope
+{
+    class ResourceMonster : IDisposable
+    {
+        public string Name = "Anonymous";
+        public void Test()
+        {
+            Console.WriteLine("{0}: Hi there.", Name);
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("{0}: Disposed.", Name);
+        }
+    }
+}
